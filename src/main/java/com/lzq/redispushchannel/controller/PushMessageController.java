eee02b2c69aacfaac4dc3bafc9d7ee586e6d855e
lzq <l7492541>
2021-03-08 11:28:23 +0800

    “Redis分布式锁注解实现”

--- src/main/java/com/lzq/redispushchannel/controller/PushMessageController.java
+++ src/main/java/com/lzq/redispushchannel/controller/PushMessageController.java
@@ -1,19 +1,22 @@
 package com.lzq.redispushchannel.controller;
 
+import com.lzq.redispushchannel.config.redisLock.RedisLockAnnotation;
+import com.lzq.redispushchannel.config.redisLock.RedisLockTypeEnum;
 import com.lzq.redispushchannel.dto.*;
 import com.lzq.redispushchannel.mapper.StadiumMapper;
 import com.lzq.redispushchannel.service.NearbyBizService;
 import com.lzq.redispushchannel.service.RedisKitService;
 import com.lzq.redispushchannel.service.RedisService;
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.BoundGeoOperations;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.awt.print.Book;
 import java.util.List;
 
 /**
@@ -33,7 +36,7 @@ public class PushMessageController {
     private RedisService redisService;
     @Autowired
     private RedisKitService redisKitService;
-
+    private static final Logger logger = LoggerFactory.getLogger(PushMessageController.class);
     @GetMapping("/pushMessage")
     public void pushMessage(String message){
         redisTemplate.convertAndSend("messagepush",message);
@@ -66,5 +69,22 @@ public class PushMessageController {
         redisService.setGeo("location",Double.valueOf("114.087773"),Double.valueOf("35.110119"),"江波车行");
         redisService.setGeo("location",Double.valueOf("114.086816"),Double.valueOf("35.109838"),"惠民超市");
         System.out.println("距离"+redisService.getDistance("location", "江波车行", "惠民超时"));
+    }
+
+
+    @RequestMapping(value = "/getRedisLock")
+    public void testRedisLock(String name) throws InterruptedException {
+        System.out.println(getLock(name));
+    }
+    @RedisLockAnnotation(typeEnum = RedisLockTypeEnum.TWO, lockTime = 3)
+    public String getLock(String name) throws InterruptedException {
+        try {
+            logger.info("睡眠执行前");
+            Thread.sleep(10000);
+            logger.info("睡眠执行后");
+        } catch (Exception e) {
+            logger.info("has some error", e);
+        }
+        return "你好！欢迎来到RedisLock："+name;
     }
 }
--- src/main/java/com/lzq/redispushchannel/controller/TestController.java
+++ src/main/java/com/lzq/redispushchannel/controller/TestController.java
@@ -10,7 +10,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import java.awt.print.Book;
 
